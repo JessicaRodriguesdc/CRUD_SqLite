50682b6fe29a70943ca3fa72873518fd4f687e6c
JessicaRodrigues <jessicarodridc@gmail.com>
2019-09-15 18:27:31 -0300

    teste da utilizacao do linq

---
+++ CrudSqLite/Linq/LinqTeste.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudSqLite.Linq
+{
+    public class LinqTeste
+    {
+        public string buscarForeach(List<string> lista, string termo)
+        {
+            foreach(string item in lista)
+            {
+                if (item.Equals(termo))
+                    return item;
+            }
+            return null;
+        }
+        public string buscarLinq(List<string> lista, string termo)
+        {
+            return (from item in lista where item.Equals(termo) select item).First();
+          
+        }
+        public List<string> buscarLinqList(List<string> lista, string termo)
+        {
+            return (from item in lista where item.Contains(termo) select item).ToList();
+        }
+
+        public string buscarLinqLambda(List<string> lista, string termo)
+        {
+            return lista.First(x => x.Equals(termo));
+        }
+
+        public List<string> buscarLinqLambdaList(List<string> lista, string termo)
+        {
+            return lista.Where(x => x.Contains(termo)).ToList();
+        }
+    }
+}
--- CrudSqLite/Program.cs
+++ CrudSqLite/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CrudSqLite.Business;
 using CrudSqLite.Dao;
+using CrudSqLite.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,34 @@ namespace CrudSqLite
     {
         static void Main(string[] args)
         {
-            ClienteBusiness cliBus = new ClienteBusiness();
+            //ClienteBusiness cliBus = new ClienteBusiness();
             //cliBus.addCli();
             //cliBus.atualizarCli();
             //cliBus.excluirCli();
-            cliBus.verRegistroCli();
+            //cliBus.verRegistroCli();
+
+            List<string> clientes = new List<string>();
+            
+            clientes.Add("emy");
+            clientes.Add("vik");
+            clientes.Add("ino");
+            clientes.Add("jsk");
+            clientes.Add("jor");
+            clientes.Add("izk");
+            clientes.Add("leo");
+            clientes.Add("seg");
+
+            LinqTeste teste = new LinqTeste();
+
+            Console.WriteLine(teste.buscarForeach(clientes, "jsk"));
+            Console.WriteLine(teste.buscarLinq(clientes, "jsk"));
+            Console.WriteLine(teste.buscarLinqLambda(clientes, "jsk"));
+
+
+            teste.buscarLinqList(clientes, "i").ForEach(x => Console.WriteLine(x));
+            teste.buscarLinqLambdaList(clientes, "i").ForEach(x => Console.WriteLine(x));
+
+
             Console.ReadKey();
         }
     }
